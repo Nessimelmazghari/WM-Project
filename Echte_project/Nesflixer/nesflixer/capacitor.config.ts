abc72fbcedd49c1532cdaea73449242e21ab3d39
Nessimelmazghari <146321282+Nessimelmazghari@users.noreply.github.com>
2024-12-19 22:10:41 +0100

    Build Errors gefixt

---
+++ Echte_project/Nesflixer/nesflixer/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.nesflixer',
+  appName: 'nesflixer',
+  webDir: 'dist'
+};
+
+export default config;
